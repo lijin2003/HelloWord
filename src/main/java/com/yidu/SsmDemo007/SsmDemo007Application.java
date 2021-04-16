e14746017548d5e5b1472ecbd5ef3b0e4db21daf
lijin2003 <2821287005@qq.com>
2021-04-16 15:47:35 +0800

    第一次提交

---
+++ src/main/java/com/yidu/SsmDemo007/SsmDemo007Application.java
@@ -0,0 +1,12 @@
+package com.yidu.SsmDemo007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmDemo007Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SsmDemo007Application.class,args);
+    }
+
+}
